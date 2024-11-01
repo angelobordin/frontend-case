28ef25e3b5f0f119b9812484d8c3bf650c56687d
Angelo Bordin <angelombordin@gmail.com>
2024-10-31 22:24:08 -0300

    adjust auth guard

--- src/Utils/auth/auth-guard.ts
+++ src/Utils/auth/auth-guard.ts
@@ -1,15 +1,14 @@
 import { Navigate } from "react-router-dom";
-import AuthService from "../../components/IBanking/Login/service";
+import AuthService from "../../components/Login/service";
 
 import { ReactNode } from "react";
 
 const authService = new AuthService();
 
 const AuthGuard = (props: { children: ReactNode }) => {
-  authService.isAuthenticated()
-    ? console.log("props.children")
-    : console.log("Navigate({ to");
-  return authService.isAuthenticated() ? props.children : Navigate({ to: "/" });
+  return authService.isAuthenticated()
+    ? props.children
+    : Navigate({ to: "/auth" });
 };
 
 export default AuthGuard;
