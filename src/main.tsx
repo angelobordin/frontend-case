e0f2b18c517d25248eba1e960aa4f426c61dce18
Angelo Bordin <angelombordin@gmail.com>
2024-11-02 10:07:06 -0300

    import bootstrap in main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./assets/styles/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
