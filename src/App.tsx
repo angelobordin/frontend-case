553b33f81e18ed11c04c00343b28d98153b2491d
Angelo Bordin <angelombordin@gmail.com>
2024-11-05 11:21:00 -0300

    implements react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,31 @@ import Home from "./Pages/Home";
 import Todo from "./Pages/Todo";
 import { IBanking } from "./Pages/IBanking";
 import { Login } from "./Pages/Login";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/todo" element={<Todo />} />
-        <Route path="/auth" element={<Login />} />
-        <Route path="/ibanking" element={<IBanking />} />
-      </Routes>
-    </Router>
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Router>
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/auth" element={<Login />} />
+          <Route path="/ibanking" element={<IBanking />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./assets/styles/styles.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
